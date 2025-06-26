4dd748f8797baa78f0a91e10a8f940326f370312
Athiraminnu <athiraminnu1999@gmail.com>
2025-06-26 10:44:12 +0530

    Resume

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import './App.css';
 function App() {
   const handleDownload = () => {
     const link = document.createElement("a");
-    link.href = "/ATHIRA A Resume.pdf";
+    link.href = `${process.env.PUBLIC_URL}/ATHIRA A Resume.pdf`;
     link.download = "Athira Resume.pdf";
     link.click();
   };
